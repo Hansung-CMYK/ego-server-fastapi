01c8400e56d1d7541baf0dba861d13562c89f4a5
MyeongjunKim0615 <audwnssp@naver.com>
2025-05-17 03:44:22 +0900

    fix: change return type

--- app/exception/exception_handler.py
+++ app/exception/exception_handler.py
@@ -1,5 +1,6 @@
 from fastapi import Request, FastAPI
-
+from fastapi.encoders import jsonable_encoder
+from starlette.responses import JSONResponse
 from app.api.common_response import CommonResponse
 import logging
 
@@ -9,15 +10,23 @@ def register_exception_handlers(app: FastAPI):
     @app.exception_handler(ControlledException)
     async def controlled_exception_handler(request: Request, exception: ControlledException):
         logging.error(f"[{request.method}] {request.url} 에서 에러 발생: {exception}")
-        return CommonResponse(
+        body = CommonResponse(
             code= exception.error_code.code,
             message= exception.error_code.message,
+        )
+        return JSONResponse(
+            status_code=400,
+            content=jsonable_encoder(body)
         )
 
     @app.exception_handler(Exception)
     async def generic_exception_handler(request: Request, exception: Exception):
         logging.error(f"[{request.method}] {request.url} 에서 에러 발생: {exception}")
-        return CommonResponse(
+        body = CommonResponse(
             code=-500,
             message="알 수 없는 에러",
+        )
+        return JSONResponse(
+            status_code=400,
+            content=jsonable_encoder(body)
         )
