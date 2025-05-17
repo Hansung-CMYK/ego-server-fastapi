ce843c5f81cfef3db4fb57418c91791a13a827db
MyeongjunKim0615 <audwnssp@naver.com>
2025-05-18 01:06:55 +0900

    feat: refactoring get_keywords_textranker

--- app/models/keyword_model.py
+++ app/models/keyword_model.py
@@ -1,11 +1,38 @@
 from keybert import KeyBERT
 from kiwipiepy import Kiwi
+from krwordrank.word import KRWordRank
 from sentence_transformers import SentenceTransformer
 
 class KeywordModel:
     __model = SentenceTransformer("snunlp/KR-SBERT-V40K-klueNLI-augSTS")  # 한국어 SBERT
     __keyword_model = KeyBERT(__model)
     __kiwi = Kiwi()
+
+    def get_keywords_textranker(self, stories: list[list[str]], count: int = 5, beta=0.85, max_iter=10):
+        # KRWordRank는 호출할 때마다 새로 생성
+        wordrank_extractor = KRWordRank(
+            min_count=1,  # 단어의 최소 출현 빈도수 (그래프 생성 시)
+            max_length=100,  # 단어의 최대 길이
+            verbose=True
+        )
+
+        # 텍스트 준비
+        texts = [chat for story in stories for chat in story]
+        full_text = " ".join(texts)
+
+        # 명사 추출
+        nouns = [
+            token.form
+            for analyzed in self.__kiwi.analyze(full_text)
+            for token in analyzed[0]
+            if token.tag.startswith("NN")
+        ]
+        result_text = " ".join(nouns)
+
+        # 추출
+        keywords, _, _ = wordrank_extractor.extract([result_text], beta=beta, max_iter=max_iter)
+
+        return [word for word, _ in sorted(keywords.items(), key=lambda x: -x[1])[:count]]
 
     def get_keywords_vector_embedding(self, stories:list[list[str]], count:int=5):
         """
@@ -18,8 +45,7 @@ class KeywordModel:
 
         for sentence in self.__kiwi.analyze(sentences):
             nouns = [token.form for token in sentence[0] if token.tag.startswith('NN')]
-            if nouns:
-                nouns_list.extend(nouns)
+            if nouns: nouns_list.extend(nouns)
         result_text = ' '.join(nouns_list)
 
         # TODO: 아키텍처(architecture)&환경(enviroment)에 따라 에러가 발생할 수 있다.
