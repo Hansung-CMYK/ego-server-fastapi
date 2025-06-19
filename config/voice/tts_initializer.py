fa922f7b8632d7625d1d05a3a35b4ad9b043c51b
MyeongjunKim0615 <audwnssp@naver.com>
2025-06-20 02:03:57 +0900

    feat: refactoring dictionary sturcture

---
+++ config/voice/tts_initializer.py
@@ -0,0 +1,4 @@
+from config.voice.tts_infer import ensure_init
+
+def load_tts_model(model_id: str, gpt_path: str, sovits_path: str) -> None:
+    ensure_init(model_id, gpt_path, sovits_path)
---
+++ config/voice/voice_chat_manager.py
@@ -0,0 +1,16 @@
+from .voice_chat_handler import VoiceChatHandler
+from ..session.session_config import SessionConfig
+
+class VoiceChatSessionManager:
+    def __init__(self):
+        self.sessions: dict[str, VoiceChatHandler] = {}
+
+    def create_session(self, ws, config : SessionConfig):
+        session = VoiceChatHandler(ws, config)
+        self.sessions[session.id] = session
+        return session
+
+    def remove_session(self, sid: str):
+        if sid in self.sessions:
+            self.sessions[sid].stop()
+            del self.sessions[sid]
