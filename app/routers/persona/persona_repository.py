4e23b92d189629e0dfd7a65ff9e8fb7712e1a816
MyeongjunKim0615 <audwnssp@naver.com>
2025-06-23 04:27:09 +0900

    feat: change repository structure

--- app/routers/persona/persona_repository.py
+++ app/routers/persona/persona_repository.py
@@ -1,121 +1,104 @@
 import json
-import threading
 
 from app.internal.exception.error_code import ControlledException, ErrorCode
-from config.common.common_database import CommonDatabase
 from config.database.postgres_database import PostgresDatabase
 
+"""
+Postgres Persona를 관리하기 위한 Respository
 
-class PersonaRepository:
+Persona와 관련된 SQL을 관리한다.
+"""
+
+database = PostgresDatabase()
+
+def insert_persona(ego_id: str, persona: dict):
     """
-    Postgres Persona를 관리하기 위한 Respository
+    요약:
+        페르소나를 추가하는 함수
 
-    Persona와 관련된 SQL을 관리한다.
+    Parameters:
+        ego_id(str): 추가할 에고의 아이디 * BE ego 테이블과 1대1 매핑되어야 한다.
+        persona(dict): 추가할 페르소나 정보
     """
-    _instance = None
-    _lock = threading.Lock()
+    database.execute_update(
+        sql="INSERT INTO persona (ego_id, persona) VALUES (%s, %s)",
+        values=(ego_id, json.dumps(persona),)
+    )
 
-    def __new__(cls, *args, **kwargs):
-        """
-        Parameters:
-            database(CommonDatabase): Database를 활용하기 위한 구현체
-        """
-        if cls._instance is None:
-            with cls._lock:
-                if cls._instance is None:
-                    instance = super().__new__(cls)
-                    instance.database = kwargs.get("database") or PostgresDatabase()
-                    cls._instance = instance
-        return cls._instance
+def update_persona(ego_id: str, persona: dict):
+    """
+    요약:
+        기존 페르소나를 변경하는 함수
 
-    def insert_persona(self, ego_id: str, persona: dict):
-        """
-        요약:
-            페르소나를 추가하는 함수
+    Parameters:
+        ego_id(str): 변경할 에고의 아이디
+        persona(dict): 새로 저장할 사용자의 페르소나\
+    """
+    database.execute_update(
+        sql = "UPDATE persona SET persona = %s WHERE ego_id = %s",
+        values=(json.dumps(persona), ego_id,)
+    )
 
-        Parameters:
-            ego_id(str): 추가할 에고의 아이디 * BE ego 테이블과 1대1 매핑되어야 한다.
-            persona(dict): 추가할 페르소나 정보
-        """
-        self.database.execute_update(
-            sql="INSERT INTO persona (ego_id, persona) VALUES (%s, %s)",
-            values=(ego_id, json.dumps(persona),)
-        )
+def select_persona_to_ego_id(ego_id: str)->tuple:
+    """
+    요약:
+        ego_id를 이용해 페르소나를 저장하는 함수
 
-    def update_persona(self, ego_id: str, persona: dict):
-        """
-        요약:
-            기존 페르소나를 변경하는 함수
+    Parameters:
+        ego_id(str): 조회할 ego의 아이디
 
-        Parameters:
-            ego_id(str): 변경할 에고의 아이디
-            persona(dict): 새로 저장할 사용자의 페르소나\
-        """
-        self.database.execute_update(
-            sql = "UPDATE persona SET persona = %s WHERE ego_id = %s",
-            values=(json.dumps(persona), ego_id,)
-        )
-
-    def select_persona_to_ego_id(self, ego_id: str)->tuple:
-        """
-        요약:
-            ego_id를 이용해 페르소나를 저장하는 함수
-
-        Parameters:
-            ego_id(str): 조회할 ego의 아이디
-
-        Raises:
-            PERSONA_NOT_FOUND: ego_id로 페르소나 조회 실패
-        """
-        result=self.database.execute_query(
-                sql="SELECT * FROM persona WHERE ego_id = %s",
-                values=(ego_id,)
-        )
-
-        if len(result) == 0:
-            raise ControlledException(ErrorCode.PERSONA_NOT_FOUND)
-        else:
-            return result[0] # 페르소나 결과 반환
-
-    def has_persona(self, ego_id: str) -> bool:
-        """
-        요약:
-            persona 테이블에 이미 ego_id가 존재하는지 확인하는 함수
-
-        Parameters:
-            ego_id(str): 존재하는지 확인힐 ego 아이디
-        """
-        result=self.database.execute_query(
+    Raises:
+        PERSONA_NOT_FOUND: ego_id로 페르소나 조회 실패
+    """
+    result=database.execute_query(
             sql="SELECT * FROM persona WHERE ego_id = %s",
             values=(ego_id,)
-        )
+    )
 
-        if len(result) == 0:
-            return False
-        else:
-            return True
+    if len(result) == 0:
+        raise ControlledException(ErrorCode.PERSONA_NOT_FOUND)
+    else:
+        return result[0] # 페르소나 결과 반환
 
-    def delete_persona(self, ego_id: str):
-        """
-        모든 데이터를 제거하는 함수
-        """
-        self.database.execute_update(
-            sql="DELETE FROM persona WHERE ego_id = %s",
-            values=(ego_id,)
-        )
+def has_persona(ego_id: str) -> bool:
+    """
+    요약:
+        persona 테이블에 이미 ego_id가 존재하는지 확인하는 함수
 
-    def create_persona(self):
-        """
-        persona 테이블을 생성하는 함수
-        """
-        self.database.execute_update(
-            sql = "CREATE TABLE persona (ego_id VARCHAR(255) PRIMARY KEY, persona JSONB NOT NULL)"
-        )
+    Parameters:
+        ego_id(str): 존재하는지 확인힐 ego 아이디
+    """
+    result=database.execute_query(
+        sql="SELECT * FROM persona WHERE ego_id = %s",
+        values=(ego_id,)
+    )
 
-    def drop_persona(self):
-        """
-        persona 테이블을 제거하는 함수
-        """
-        self.database.execute_update(
-            sql = "DROP TABLE persona"
-        )
+    if len(result) == 0:
+        return False
+    else:
+        return True
+
+def delete_persona(ego_id: str):
+    """
+    모든 데이터를 제거하는 함수
+    """
+    database.execute_update(
+        sql="DELETE FROM persona WHERE ego_id = %s",
+        values=(ego_id,)
+    )
+
+def create_persona():
+    """
+    persona 테이블을 생성하는 함수
+    """
+    database.execute_update(
+        sql = "CREATE TABLE persona (ego_id VARCHAR(255) PRIMARY KEY, persona JSONB NOT NULL)"
+    )
+
+def drop_persona():
+    """
+    persona 테이블을 제거하는 함수
+    """
+    database.execute_update(
+        sql = "DROP TABLE persona"
+    )
--- app/routers/tone/tone_repository.py
+++ app/routers/tone/tone_repository.py
@@ -5,81 +5,67 @@ from config.common.common_database import CommonDatabase
 from config.database.postgres_database import PostgresDatabase
 
 
-class ToneRepository:
+"""
+Postgres Tone을 호출하기 위한 Respository
+
+Tone과 관련된 SQL을 관리한다.
+"""
+
+database = PostgresDatabase()
+
+def insert_tone(ego_id: str, tone: dict):
     """
-    Postgres Tone을 호출하기 위한 Respository
+    요약:
+        말투를 추가하는 함수
 
-    Tone과 관련된 SQL을 관리한다.
+    Parameters:
+        ego_id(str): 추가할 에고의 아이디 * BE ego 테이블과 1대1 매핑되어야 한다.
+        tone(dict): 추가할 말투 정보
     """
-    _instance = None
-    _lock = threading.Lock()
+    database.execute_update(
+        sql="INSERT INTO tone (ego_id, tone) VALUES (%s, %s)",
+        values = (ego_id, json.dumps(tone),)
+    )
 
-    def __new__(cls, *args, **kwargs):
-        """
-        Parameters:
-            database(CommonDatabase): Database를 활용하기 위한 구현체
-        """
-        if cls._instance is None:
-            with cls._lock:
-                if cls._instance is None:
-                    instance = super().__new__(cls)
-                    instance.database = kwargs.get("database") or PostgresDatabase()
-                    cls._instance = instance
-        return cls._instance
+def has_tone(ego_id: str) -> bool:
+    """
+    요약:
+        tone 테이블에 이미 ego_id가 존재하는지 확인하는 함수
 
-    def insert_tone(self, ego_id: str, tone: dict):
-        """
-        요약:
-            말투를 추가하는 함수
+    Parameters:
+        ego_id(str): 존재하는지 확인힐 ego 아이디
+    """
+    result=database.execute_query(
+        sql="SELECT * FROM tone WHERE ego_id = %s",
+        values=(ego_id,)
+    )
+    if len(result) == 0: return False
+    else: return True
 
-        Parameters:
-            ego_id(str): 추가할 에고의 아이디 * BE ego 테이블과 1대1 매핑되어야 한다.
-            tone(dict): 추가할 말투 정보
-        """
-        self.database.execute_update(
-            sql="INSERT INTO tone (ego_id, tone) VALUES (%s, %s)",
-            values = (ego_id, json.dumps(tone),)
-        )
+def delete_tone(ego_id: str):
+    """
+    모든 데이터를 제거하는 함수
 
-    def has_tone(self, ego_id: str) -> bool:
-        """
-        요약:
-            tone 테이블에 이미 ego_id가 존재하는지 확인하는 함수
+    Parameters:
+        ego_id: 제거할 ego_id
+    """
+    database.execute_update(
+        sql="DELETE FROM tone WHERE ego_id = %s",
+        values=(ego_id,)
+    )
 
-        Parameters:
-            ego_id(str): 존재하는지 확인힐 ego 아이디
-        """
-        result=self.database.execute_query(
-            sql="SELECT * FROM tone WHERE ego_id = %s",
-            values=(ego_id,)
-        )
-        if len(result) == 0: return False
-        else: return True
+def create_tone():
+    """
+    tone 테이블을 생성하는 함수
+    """
+    database.execute_update(
+        sql="CREATE TABLE tone (ego_id VARCHAR(255) PRIMARY KEY, tone JSONB NOT NULL)"
+    )
 
-    def delete_tone(self, ego_id: str):
-        """
-        모든 데이터를 제거하는 함수
-
-        Parameters:
-            ego_id: 제거할 ego_id
-        """
-        self.database.execute_update(
-            sql="DELETE FROM tone WHERE ego_id = %s",
-            values=(ego_id,)
-        )
-
-    def create_tone(self):
-        """
-        tone 테이블을 생성하는 함수
-        """
-        self.database.execute_update(
-            sql="CREATE TABLE tone (ego_id VARCHAR(255) PRIMARY KEY, tone JSONB NOT NULL)"
-        )
-
-    def drop_tone(self):
-        """
-        tone 테이블을 제거하는 함수
-        """
-        self.database.execute_update(
-            sql="DROP TABLE tone"
-        )
+def drop_tone():
+    """
+    tone 테이블을 제거하는 함수
+    """
+    database.execute_update(
+        sql="DROP TABLE tone"
+    )
